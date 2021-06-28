641f695ce85bc67404dafb5841aa23338432ed85
Scott Hansen <scottiehansen@gmail.com>
2021-06-28 09:00:23 -0700

    Basic get request working with sql file

---
+++ server/controller.js
@@ -0,0 +1,16 @@
+const db = require('../db/connection.js');
+
+let controller = {
+  getReview: (req, res) => {
+    var queryString = `SELECT * FROM reviews WHERE product_id = 5`
+    db.query(queryString, (err, results) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).send(results);
+      }
+    })
+  }
+}
+
+module.exports = controller;
--- server/router.js
+++ server/router.js
@@ -1,1 +1,9 @@
+let express = require("express");
+let Router = express.Router();
+let controller = require("./controller.js");
 
+Router
+  .route('/reviews')
+    .get(controller.getReview)
+
+module.exports = Router;
